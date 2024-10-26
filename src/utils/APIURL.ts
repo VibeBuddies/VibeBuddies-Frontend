5e05c3a171502badc175e34cd5eae02b6e105371
Nick <93751758+kofflernick@users.noreply.github.com>
2024-10-26 14:36:38 -0400

    merge

---
+++ src/utils/APIURL.ts
@@ -0,0 +1,3 @@
+export const API_URL = "http://3.81.121.171:3000"
+
+// module.exports = {API_URL}
---
+++ src/utils/saveToken.ts
@@ -0,0 +1,13 @@
+let token = ""
+const setLocalToken = (newToken: string) => {
+  token = newToken
+}
+
+const getToken = () => {
+  return token
+}
+
+module.exports = {
+  getToken,
+  setLocalToken,
+}
